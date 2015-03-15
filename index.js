ed4caeda2db66e7440c5100d9c154e854a261999
Rein van 't Veer <rein@baksteen.bureaudigitaalerfgoed.nl>
2015-03-15 19:44:38 +0100

    first working version with async and gm modules, no bzip support yet

--- index.js
+++ index.js
@@ -5,40 +5,62 @@ Usage: node index.js -infiles '/images/image*.tiff.bzip' -outdir /thumbs -width 
 
 var argv = require('optimist')
     .usage('Create image thumbnails from .\nUsage: $0')
-    .demand(['indir', 'outdir', 'compression', 'width', 'height'])
-    .describe('f', 'Load a file')
+    .demand(['infiles', 'outdir', 'compression', 'width', 'height'])
     .argv,
     fs = require('fs'),
     path = require('path'),
-    imageMagick = require('imageMagick'),
+    gm = require('gm'),
     glob = require('glob'),
-    bunzip = require('seek-bzip');
+    bunzip = require('seek-bzip'),
+    async = require('async');
 
-var pictureFileName,
+var options,
+	pictureFileName,
     compressedData,
     data;
 
-glob(argv.infiles, function (er, infiles) {
-    'use strict';
-    infiles.forEach(function (file) {
+options = {
+			nocase: true
+		};
 
-        if (file.name.substr(file.name.length - 4, file.name.length) === ".bzip") {
-            compressedData = fs.readFileSync(file.path);
-            data = Bunzip.decode(compressedData);
-            pictureFileName = file.name.substr(0, file.name.length - 4);
-            fs.writeFileSync(pictureFileName, data);
-        } else {
-            pictureFileName = file.name;
-        }
+var filelist = glob.sync(argv.infiles, options);
+console.log(filelist.length);
 
-        imageMagick(pictureFileName)
-            .resize(argv.width, argv.height, '^')
-            .gravity('center')
-            .extent(argv.width, argv.height)
-            .write(argv.outdir, function (error) {
-                if (error) {
-                    console.log(error);
-                }
-            });
-    });
-});
+async.eachSeries(
+	filelist, 
+	function (file, callback) {
+		'use strict';
+		console.log(file);
+		
+		/*
+
+		if (file.name.substr(file.name.length - 4, file.name.length) === ".bzip") {
+			compressedData = fs.readFileSync(file.path);
+			data = Bunzip.decode(compressedData);
+			pictureFileName = file.name.substr(0, file.name.length - 4);
+			fs.writeFileSync(pictureFileName, data);
+		} else {
+			pictureFileName = file.name;
+		}
+		*/
+
+		var pictureFileName = String(file);
+		var outfilename = pictureFileName.split('/');
+		outfilename = outfilename[outfilename.length - 1];
+		
+		gm(pictureFileName).resize(argv.width, argv.height).write(argv.outdir + '/' + outfilename, function (err) {
+			if (err) {
+				console.log(err);
+				callback(err); 
+			} else {
+				console.log('Wrote ' + argv.outdir + outfilename);
+				callback();
+			}
+		});
+	}, 
+	function (err) {
+		if (err) {
+			fs.appendFileSync('errors.txt', new Date().toLocaleString() + " " + pictureFileName + ": " + error + "\n");
+		}
+	}
+);
